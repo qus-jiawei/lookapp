d9879dc2f7f93b55176d91cad12eae3f0816684d
qus-jiawei <qus-jiawei@gmail.com>
2013-12-16 11:11:53 +0800

    调整目录结构

--- kill_nohup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps aux|grep python|grep productrun|grep -v grep|awk '{print $2}'|xargs kill
--- nohup_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/hadoop/yarn_monitor/lookapp/webpython
-nohup flask/bin/python productrun.py  >>  /home/hadoop/yarn_monitor/logs/web.log  &
