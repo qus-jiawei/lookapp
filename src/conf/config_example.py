78293c09e59abd0cf2a630d20c1b487d722ad77e
qus-jiawei <qus-jiawei@gmail.com>
2013-12-16 13:45:22 +0800

    change config

--- src/conf/config_example.py
+++ src/conf/config_example.py
@@ -4,16 +4,11 @@
 #web监听的端口
 listen_port=59999
 
-# nodedata:存放look.sh采集的数据目录
-nodedata = "/home/qiujw/MyHadoop/lookapp/nodedata"
-
 # log 's path
 logpath = "d:/webpython/logs"
 
 # sqllite db 's path
 sqlitepath = "d:/sqllite.db"
-#!python
-# coding=utf8
 
 # node manager's port
 nmport = 59842
@@ -22,19 +17,15 @@ nmport = 59842
 rmhost = "mob616"
 rmport = "50088"
 
-# rmhost="hadoop2"
-# rmport="59088"
-
 # resource manager's host and port
 hshost = "mob616"
 hsport = "50888"
 
-# hshost="hadoop1"
-# hsport="59888"
-
 # collect run interval
+# unchangeable
 collect_interval = 600
 
+#nodemanager's list
 hosts = ["kpi11", "kpi12", "kpi13", "kpi14", "kpi15", "kpi17",
        "kpi18", "kpi19", "kpi25", "kpi26", "kpi27", "kpi28", 
        "kpi39", "kpi40", "kpi41", "kpi42", "kpi43", "kpi44", 
